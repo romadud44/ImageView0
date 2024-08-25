1e3c1b21fe1968825f7e0e9276b4e77c71f774a0
romadud44 <roman_crt-service@mail.ru>
2024-08-25 20:57:14 +0300

    commit

--- arx/MyApplicationTestSavedInstanceState/app/src/main/java/com/example/myapplicationtestsavedinstancestate/CountViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplicationtestsavedinstancestate
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class CountViewModel:ViewModel() {
-    var number = 0
-    val currentNumber: MutableLiveData<Int> by lazy{MutableLiveData<Int>()}
-    }
--- arx/TimeCalculator/app/src/main/java/com/example/timecalculator/Operation.kt
+++
@@ -1,6 +0,0 @@
-package com.example.timecalculator
-
-class Operation(private val first: Int, private val second: Int) {
-fun sum() = first + second
-fun sub() = first - second
-}
